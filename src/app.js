4486d68d25e1c09deac8a7ae3faab02191bee7f7
stewybenz95 <113649226+stewybenz95@users.noreply.github.com>
2023-05-12 00:54:55 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,40 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  button();
+  cardGenerator();
+};
+function button() {
+  let getNewCard = document.querySelector("#button");
+  getNewCard.addEventListener("click", function() {
+    cardGenerator();
+  });
+}
+let cardGenerator = () => {
+  let randomSuit = [":diamonds:", ":clubs:", ":hearts:", ":spades:"];
+  let randomNumber = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "J",
+    "A"
+  ];
+  let suit = randomSuit[Math.floor(Math.random() * randomSuit.length)];
+  let number = randomNumber[Math.floor(Math.random() * randomNumber.length)];
+  if (suit == ":hearts:" || suit == ":diamonds:") {
+    document.querySelector("#icon").style.color = "red";
+  } else {
+    document.querySelector("#icon").style.color = "black";
+  }
+  document.querySelector("#top").innerHTML = suit;
+  document.querySelector("#number").innerHTML = number;
+  document.querySelector("#bottom").innerHTML = suit;
 };
